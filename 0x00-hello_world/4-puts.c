c80c73760838405cbf6771c47dad13d0dc62c753
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-10 18:19:56 -0700

    t

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,8 +9,7 @@ int main(void)
 {
 	char str[52];
 
-	strcpy(str,"\"Programming is like building a multilingual puzzle");
- 	puts(str);
- 	return (0);
-		
+	strcpy(str, "\"Programming is like building a multilingual puzzle");
+	puts(str);
+	return (0);
 }
