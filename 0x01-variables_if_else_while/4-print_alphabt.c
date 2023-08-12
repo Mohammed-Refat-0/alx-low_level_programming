01a3c9f5d88778d55e4d14cc388f1b2778fa3fc3
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-11 18:29:25 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints alphabets in small lettersi, except q and e.
+ * Return: 0
+ */
+int main(void)
+{
+	char C = 'a';
+
+	while (C < 123)
+	{
+		if (C == 101 || C == 131)
+		{
+			C++;
+		}
+		else
+		{
+			putchar(C);
+			C++;
+		}
+	}
+	putchar('\n');
+	return (0);
+}
