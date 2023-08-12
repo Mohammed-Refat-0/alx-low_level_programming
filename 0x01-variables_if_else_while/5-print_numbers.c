db004bbfee3f4ee84dead4cb50cb431fd91812a0
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-11 18:54:41 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - prints all single digit numbers
+ * Return: 0
+ */
+int main(void)
+{
+	for (int i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+}
