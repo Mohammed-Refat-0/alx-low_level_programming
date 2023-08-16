a589adaeaea65f16bb62cb6052b9ddf82b26a87e
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-16 05:24:58 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,13 +4,14 @@
  * prints- function that prints "_putchar"
  * Return: 0
  */
-int main(void) 
+int main(void)
 {
 	char out[] = "_putchar";
 	int i;
+
 	for (i = 0; i <= 7; i++)
 	{
-		_putchar(out[i]);
+		_putchiar(out[i]);
 	}
 	_putchar('\n');
 	return (0);
