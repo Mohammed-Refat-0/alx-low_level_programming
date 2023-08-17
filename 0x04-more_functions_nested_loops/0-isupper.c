938f9714969139881cfc0f409401a8acb72a3a0f
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-17 07:49:07 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,19 +1,22 @@
-include "main.h"
+#include "main.h"
 /**
-*_isupper - checks if a char is uppercase
+* _isupper - checks if a char is lower case
 *Return: 1 if uppercase, otherwise 0
 *@c : input char to be verified
 */
 int _isupper(int c)
 {
-	int i;
 	int k = (int)c;
+	int i;
 
 	for (i = 65; i < 91; i++)
 	{
 		if (k == i)
 		{
 			return (1);
+		}
+		else
+		{
 		}
 	}
 	return (0);
