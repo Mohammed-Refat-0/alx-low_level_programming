0174804454e4abb3395d972302b1990b3907e25e
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-16 17:56:29 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,18 +9,18 @@ int print_sign(int n)
 
 	if (n > 0)
 	{
-		_putchar("+");
+		_putchar('+');
 		return (1);
 	}
 	if else (n == 0)
 	{
-		_putchar("0");
+		_putchar('0');
 		return (0);
 
 	}
 	else
 	{
-		_putchar("-");
+		_putchar('-');
 		return (-1);
 	}
 }
