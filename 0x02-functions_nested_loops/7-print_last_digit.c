44da34f6c77724c03444745c58acdf8921af3a27
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-16 18:36:25 -0700

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* print_last_digit - return and print last digit of a number
+*Return: 0
+*@a : value to be returned and printed
+*/
+int print_last_digit(int a)
+{
+	a = a % 10;
+	_putchar('a');
+	return (a);
+}
