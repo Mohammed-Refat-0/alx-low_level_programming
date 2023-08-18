767bf4f90dc9ad343d494436850dd83693cbf037
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-17 22:02:33 -0700

    task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ * return: void
+ * @size: size of the triangle
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+
+	if (size < 1)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (int i = 0; i < size; i++)
+		{
+			for (int j = size; j > i + 1; j--)
+			{
+				_putchar(' ');
+			}
+
+				for (int k = 0; k <= i; k++)
+				{
+					_putchar('#');
+				}
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
