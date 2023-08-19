adeaeebec8edad5afacf0056ac9c20a544a42d4f
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-18 23:58:13 -0700

    task 12

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -13,9 +13,9 @@ void print_times_table(int n)
 	}
 	else
 	{
-		for (i = 0; i < n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (j = 0; j < n; j++)
+			for (j = 0; j <= n; j++)
 			{
 				if(i * j >= 100)
 				{
