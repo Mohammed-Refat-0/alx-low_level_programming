ef762e547da26658ba9ed708308c18b73b1e3c32
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-19 04:34:22 -0700

    task 15

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - program that finds the sum of eve numbers in 
+ *  fibonacci series of value below 4 million
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned long a = 1;
+	unsigned long b = 2;
+	unsigned long c;
+	unsigned long sum = 0;
+	int i;
+
+	while (c < 4000000)
+	{
+		c = a + b;
+		if (c % 2 == 0)
+		{
+			sum = sum + c;
+		}
+		a = b;
+		b = c;
+	}
+	printf("%ld\n", sum); 
+	return (0);
+}
