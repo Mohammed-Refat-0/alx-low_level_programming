6c2002eeb9500c4e736f1addbcb0918d45c50f0d
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-19 04:45:35 -0700

    task 14

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -6,15 +6,15 @@
  */
 int main(void)
 {
-	unsigned long a = 1;
-	unsigned long b = 2;
+	unsigned long a = 0;
+	unsigned long b = 1;
 	unsigned long c;
 	unsigned long sum = 0;
 
-	while (c < 4000000)
+	while (c <= 4000000)
 	{
 		c = a + b;
-		if (c % 2 == 0)
+		if ((c % 2) == 0)
 		{
 			sum = sum + c;
 		}
