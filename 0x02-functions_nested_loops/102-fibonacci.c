f637b5d59460ca995ec73040402493f6b30bdf33
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-19 00:47:15 -0700

    task 13

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - program that prints the fibonacci series of 50
+ * Return: 0
+ */
+int main(void)
+{
+	int a = 0;
+	int b = 1;
+	for (i = 0; i < 50; i++)
+	{
+		a++
+		b++
+		if (i !=50)
+		{
+			printf("%d, ", a+b);
+		}
+	}
+	printf("\n")
+	return (0);
+}
