1e225f0f8de8d051b8670f1344f7244af5ea128a
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-21 08:13:36 -0700

    task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+nclude "main.h"
+/**
+ * int _strlen - return length of string
+ * @s: string to evalute
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (s)
+	{
+		length++;
+		*s++;
+	}
+}
