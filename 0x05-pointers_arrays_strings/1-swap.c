01c2e59a14594cb8536f827b02a59629d91cd0e9
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-21 08:01:13 -0700

    task 1

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
  * swap_int- swap the value of two vars
- * @a,@b : vaules to swap
+ * @a: pointer to first value
+ * @b: pointer to second value
  *Return: void
  */
 void swap_int(int *a, int *b)
