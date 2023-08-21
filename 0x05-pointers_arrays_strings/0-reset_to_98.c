3930bbd81efb17a0afdc9f0f06c77d85e2e549f5
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-21 07:45:46 -0700

    task 0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * reset_to_98 - updates the value pointed by the ptr
- *@n: pointer to the variable to update 
+ *@n: pointer to the variable to update
  * Return: void
  */
 void reset_to_98(int *n)
