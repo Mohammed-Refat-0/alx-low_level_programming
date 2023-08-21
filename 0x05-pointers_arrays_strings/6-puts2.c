bfc168726be767a38e2e1fd047da1c65850d8a83
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-21 10:16:46 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ *_puts2 - print every other char in string
+ *@str: string
+ * Return: void
+ */
+void puts2(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+		if (*str == '\0')
+		{
+			break;
+		}
+		else
+		{
+			str++;
+		}
+	}
+	_putchar('\n');
+}
