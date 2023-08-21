fcda15f741bfb66441ac3c9e759157a10a6bfe39
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-21 10:53:26 -0700

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _strcpy: copy content of string to another
+ * @dest: destination
+ * @src: source
+ * Return: char
+ */
+char *_strcpy(char *dest, char *src)
+{
+	while(*src != '\0')
+	{
+		*dest = *src;
+		dest++;
+		src++;
+	}
+	*dest = '\0';
+	return (*dest);
+}
