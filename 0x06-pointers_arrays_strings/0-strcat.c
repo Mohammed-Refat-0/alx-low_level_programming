8620daf46c4c35b67c9718cc57b823ed38ada780
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-23 06:00:38 -0700

    task 0

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strcat - concatenates two strings.
+ * @src:prt to string
+ * @dest: ptr to stringg
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int length = 0;
+	int i;
+
+	while (dest[length] = '\0')
+	{
+		length++;
+	}
+	for (i = 0; src[i] != '\0', i++)
+	{
+		dest[length] = src[i];
+		length++;
+	}
+	dest[length] = '\0';
+	return (dest);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef MYHEADERS_H
+#define MYHEADERS_H
+int _putchar(char c);
+int add(int a, int b);
+char *_strcat(char *dest, char *src);
+#endif
