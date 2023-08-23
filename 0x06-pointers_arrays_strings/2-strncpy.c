336fcc6a36c17ff089482c825d8da44e60ca9b06
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-23 08:26:04 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -23,7 +23,7 @@ char *_strncpy(char *dest, char *src, int n)
 			dest[i] = src[i];
 			j++;
 		}
-		for (i = j; i < (n - length); i++)
+		for (i = j + 1; i < (n - length); i++)
 		{
 			dest[i] = '\0';
 		}
