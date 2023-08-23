6330305ab6de2720fab4bfe210acad7825cdc2f4
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-23 09:50:05 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,22 +1,21 @@
 #include "main.h"
-
 /**
  * string_toupper - change lowercase letters to uppercase
- * @*: pointer
- * Return: ptr to string
+ * @ptr: pointer
+ * Return: string
  */
-char *string_toupper(char *)
+char *string_toupper(char *ptr)
 {
 
 	int length = 0;
 
-	while (*[length] != '\0')
+	while (ptr[length] != '\0')
 	{
-		if  (*[length] > 90)
+		if  (ptr[length] >= 97 && ptr[length] <= 122)
 		{
-			*[length] = *[length] - 32;
+			ptr[length] = ptr[length] - 32;
 		}
 		length++;
 	}
-	return (*);
+	return (ptr);
 }
