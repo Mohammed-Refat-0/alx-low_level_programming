df9ca43c3fef582eed66ab886d9ae670904bf35a
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-23 09:14:58 -0700

    task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses an array
+ * @a: array to be reversed
+ * @n: number of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int i, j, b;
+
+	j = n - 1;
+
+	for (i = 0; i < n / 2; i++)
+	{
+		b = a[i];
+		a[i] = a[j];
+		a[j] = b;
+		j--;
+	}
+}
