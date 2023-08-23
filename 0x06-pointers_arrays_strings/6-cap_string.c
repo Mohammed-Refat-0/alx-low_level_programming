fa8c1c38c3b4d3841875f8f21c621187fcce7d7f
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-23 10:34:17 -0700

    yask 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,19 +12,21 @@ char *cap_string(char *ptr)
 	{
 		if (ptr[0] >= 97 && ptr[0] <= 122)
 		{
-			ptr[0] = ptr[0] - 32; 
+			ptr[0] = ptr[0] - 32;
+			length++;
 		}
-		if (ptr[length] == 44 || ptr[length] == 59 || ptr[length] == 46 
-			|| ptr[length] == 33 || ptr[length] == 63 || ptr[length] == 34 
-			|| ptr[length] == 40 || ptr[length] == 41 || ptr[length] == 123 || ptr[length] == 125
-			|| ptr[length] == 9 || ptr[length] == 10 || ptr[length] == 32)
+		if (ptr[length] == 44 || ptr[length] == 59 || ptr[length] == 46
+			|| ptr[length] == 33 || ptr[length] == 63 || ptr[length] == 34
+			|| ptr[length] == 40 || ptr[length] == 41 || ptr[length] == 123 ||
+			ptr[length] == 125 || ptr[length] == 9 || ptr[length] == 10
+			|| ptr[length] == 32)
 		{
 			length++;
 			if (ptr[length] >= 97 && ptr[length] <= 122)
 			{
 				ptr[length] = ptr[length] - 32;
 			}
-			}
+		}
 		length++;
 	}
 	return (ptr);
