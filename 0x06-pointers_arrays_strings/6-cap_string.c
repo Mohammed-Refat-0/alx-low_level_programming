9363818342d60c0e685427c0543b6e8836f1b78b
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-23 10:17:15 -0700

    task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * cap_string - capitalizes all words of a string.
+ * @ptr: pointer
+ * Return: string
+ */
+char *cap_string(char *ptr);
+{
+	int length = 0;
+
+	while (ptr[length] != '\0')
+	{
+		if (ptr[0] >= 97 && ptr[0] <= 122)
+		{
+			ptr[0] = ptr[0] - 32; 
+		}
+		if (ptr[length] == 44 || ptr[length] == 59 || ptr[length] == 46 
+		|| ptr[length] == 33 || ptr[length] == 63 || ptr[length] == 34 
+		|| ptr[length] == 40 || ptr[length] == 41 || ptr[length] == 123 || ptr[length] == 125)
+		{
+		if (ptr[length] >= 97 && ptr[length] <= 122)
+		{
+			ptr[length] = ptr[length] - 32;
+		}
+		}
+		length++;
+	}
+	return (ptr);
+}
