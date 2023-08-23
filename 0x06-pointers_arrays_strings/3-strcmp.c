b9abc70a36caf7ba3bfa094db6c4cc11786fd71b
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-23 08:44:10 -0700

    task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strcmp - compare two strings
+ * @s1: string 1 to compare
+ * @s2: string 2 to compare
+ * Return: 0, 1 0r -1 
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int length = 0;
+
+	while (s1[length] != '\0' && s2[length] != '\0')
+	{
+		if (s1[length] == s2[length])
+		{
+		}
+		else if(s1[length] > s2[length])
+		{
+			return (1);
+		}
+		else
+		{
+			return (-1);
+		}
+		length++;
+	}
+	return (0);
+}
