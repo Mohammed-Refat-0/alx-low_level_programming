e509c9ba5021c0a2fef0e714ccd5e6b08f46ce32
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-24 04:28:02 -0700

    task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -15,7 +15,7 @@ char *cap_string(char *ptr)
 			ptr[0] = ptr[0] - 32;
 			length++;
 		}
-		if (ptr[length] == 44 || ptr[length] == 59 || ptr[length] == 46
+		while (ptr[length] == 44 || ptr[length] == 59 || ptr[length] == 46
 			|| ptr[length] == 33 || ptr[length] == 63 || ptr[length] == 34
 			|| ptr[length] == 40 || ptr[length] == 41 || ptr[length] == 123 ||
 			ptr[length] == 125 || ptr[length] == 9 || ptr[length] == 10
