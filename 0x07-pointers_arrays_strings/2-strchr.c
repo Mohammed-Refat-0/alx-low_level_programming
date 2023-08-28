47a98fdb4e77fa9edd89cc53c497fedbac4bee05
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-28 11:11:39 -0700

    task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a char in string
+ * @s: ptr to string to search in
+ * @c: char to locate
+ * Return: pointer to the memory area of located char or null if not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+	char *ptr;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			ptr = &s[i];
+			return (ptr);
+		}
+	}
+
+	return (NULL);
+}
