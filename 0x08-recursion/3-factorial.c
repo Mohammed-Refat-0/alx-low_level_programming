4faeca83564f3026606dbbf4bb4978cd555231a5
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-30 11:45:41 -0700

    task 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ *factorial - returns the factorial of a number
+ * @n: number to return the fctorial of.
+ * Return: factorial integer
+*/
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
