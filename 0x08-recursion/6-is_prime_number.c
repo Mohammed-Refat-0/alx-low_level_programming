2dfb7da6fbb00bf32ae44fae0a1e59ae27545d98
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-30 12:55:23 -0700

    task

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * is_prime_number - evalute if an integer is an prime or not
+ @n : integer to evalute
+ * Return: int
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (pprime(n, n - 1));
+}
+
+/**
+ * pprime - calculates if a number is prime
+ * @n: integer to evaluate
+ * @i: iterator
+ * Return: int
+ */
+int pprime(int n, int i)
+{
+	if (i == 1)
+		return (1);
+	if (n % i == 0 && i > 0)
+		return (0);
+	return (pprime(n, i - 1));
+}
