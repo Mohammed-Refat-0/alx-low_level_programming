f87c052fe47b188a31eeae314aa03fcca7363984
Mohammed Refat <mohammedrefat53@outlook.com>
2023-08-30 03:31:13 -0700

    task

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -24,7 +24,7 @@ void print_diagsums(int *a, int size)
 				sum1 = sum1 + a[((i * size) + j)];
 			}
 		}
-		sum2 = sum2 + a[(size * i) + (j - i)];
+		sum2 = sum2 + a[((size * i) + (j - i))];
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
