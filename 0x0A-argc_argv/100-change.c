64c1c306d32b87d11dd1f5109bbd58556abeb4bd
Mohammed Refat <mohammedrefat53@outlook.com>
2023-09-01 08:11:39 -0700

    f

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,7 +8,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int cents;
+	int cents, i;
 	int coins = 0;
 
 	if (argc != 2)
@@ -17,29 +17,29 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 	cents = atoi(argv[1]);
-	for(i = 0; cents < 0; i++)
+	for (i = 0; cents < 0; i++)
 	{
 		if ((cents - 25) >= 0)
 		{
-			cents -= 25;
+			cents = cents - 25;
 			continue;
 		}
 		if ((cents - 10) >= 0)
 		{
-			cents -= 10;
+			cents = cents - 10;
 			continue;
 		}
 		if ((cents - 5) >= 0)
 		{
-			cents -= 5;
+			cents = cents - 5;
 			continue;
 		}
 		if ((cents - 2) >= 0)
 		{
-			cents -= 2;
+			cents = cents - 2;
 			continue;
 		}
-		conis++
+		coins++;
 		cents--;
 	}
 
