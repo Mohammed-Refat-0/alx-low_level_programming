fd40f5d7b41c206604796ca34926646f59d1fc6e
Mohammed Refat <mohammedrefat53@outlook.com>
2023-09-01 06:22:42 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - prints the name of the program
+ * @argc: number of arguments
+ * @argv: array of string arguments
+ * Return: 0
+ */
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,7 @@
+#ifndef MYHEADERS_H
+#define MYHEADERS_H
+
+int add(int a, int b);
+int _putchar(char c);
+void print_alphabet(void);
+#endif
