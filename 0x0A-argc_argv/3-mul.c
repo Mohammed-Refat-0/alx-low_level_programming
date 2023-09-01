38c60072d8b7ff0235feff38e85beeaf30eebedb
Mohammed Refat <mohammedrefat53@outlook.com>
2023-09-01 07:27:30 -0700

    t

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
 /**
  * main - multiple and  prints two numbers
  * @argc: number of arguments
@@ -9,14 +8,16 @@
  */
 int main(int argc, char *argv[])
 {
+	int n1, n2, c;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
-		return(1);
+		return (1);
 	}
-	int n1 = atoi(argv[1]);
-	int n2 = atoi(argv[2]);
-	int c = n1 * n2;
+	n1 = atoi(argv[1]);
+	n2 = atoi(argv[2]);
+	c = n1 * n2;
 	printf("%d\n", c);
 	return (0);
 }
