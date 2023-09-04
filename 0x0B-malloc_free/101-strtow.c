750e305d6e831210cec178119dfd95d51d8a2140
mohammedrefat <mohammedrefat53@outlook.com>
2023-09-04 22:31:23 +0300

    task

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include "main.h"
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ * Return: pointer to an array of strings
+ */
+char **strtow(char *str)
+{
+	if (!str)
+	{
+		return (NULL);
+	}
+	return (NULL);
+}
