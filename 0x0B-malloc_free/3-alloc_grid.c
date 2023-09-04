3d03410c6d4c9c377e53d6b64e957b2d3e20da02
mohammedrefat <mohammedrefat53@outlook.com>
2023-09-04 20:41:32 +0300

    task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,7 +15,7 @@ int **alloc_grid(int width, int height)
 	{
 		return (NULL);
 	}
-	ptr = (int **) malloc((height) * sizeof(int*));
+	ptr = (int **) malloc((height) * sizeof(int *));
 	if (!ptr)
 	{
 		return (NULL);
@@ -33,7 +33,7 @@ int **alloc_grid(int width, int height)
 	}
 	for (i = 0; i < height ; i++)
 	{
-		for (j = 0; j < width; width++)
+		for (j = 0; j < width; j++)
 		{
 			ptr[i][j] = 0;
 		}
