c6617f6caecfce6264617a55432c8c995cbda8ad
mohammedrefat <mohammedrefat53@outlook.com>
2023-09-05 22:16:11 +0300

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,56 +1,70 @@
 #include <stdlib.h>
-#include "main.h"
-/**
- * **strtow - splits a string into words
- * @str: string to split
- * Return: pointer to an array of strings
- */
+
 char **strtow(char *str)
 {
-	char **ptr;
-	int i, j;
-	int words = 0;
-	int c = 0;
+    char **ptr;
+    int i, j, k;
+    int words = 0;
+    int c = 0;
 
-	if (!str || *str == ' '  || *str == '\0')
-	{
-		return (NULL);
-	}
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] == ' ')
-		{
-			words++;
-		}
-	}
-	ptr = (char **) malloc((words + 1) * sizeof(char *));
-	if (!ptr)
-	{
-		return (NULL);
-	}
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (str[i] == 32)
-		{
-			ptr[i] = (char *) malloc((c + 1) * sizeof(char));
-			if (!ptr[i])
-			{
-				return (NULL);
-			}
-			c = 0;
-		}
-		c++;
-	}
-	ptr[words] = NULL;
-
-	for (i = 0; ptr[i] != NULL; i++)
-	{
-		for (j = 0; str[j] != ' '; j++)
-		{
-			ptr[i][j] = str[j];
-		}
-		ptr[i][j] = '\0';
-		j++;
-	}
-	return (ptr);
+    if (!str || *str == '\0')
+    {
+        return NULL;
+    }
+    
+    // Count the number of words in the string
+    for (i = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == ' ')
+        {
+            words++;
+        }
+    }
+    
+    // Increment words if the last character is not a space
+    if (str[i - 1] != ' ')
+    {
+        words++;
+    }
+    
+    // Allocate memory for the ptr array
+    ptr = (char **) malloc((words + 1) * sizeof(char *));
+    if (!ptr)
+    {
+        return NULL;
+    }
+    
+    // Allocate memory for each word and copy characters
+    for (i = 0, j = 0, k = 0; str[i] != '\0'; i++)
+    {
+        if (str[i] == ' ')
+        {
+            ptr[j] = (char *) malloc((k + 1) * sizeof(char));
+            if (!ptr[j])
+            {
+                return NULL;
+            }
+            ptr[j][k] = '\0';
+            j++;
+            k = 0;
+        }
+        else
+        {
+            ptr[j][k] = str[i];
+            k++;
+        }
+    }
+    
+    // Handle the last word
+    ptr[j] = (char *) malloc((k + 1) * sizeof(char));
+    if (!ptr[j])
+    {
+        return NULL;
+    }
+    ptr[j][k] = '\0';
+    
+    // Set the last element of the array to NULL
+    ptr[words] = NULL;
+    
+    return ptr;
 }
