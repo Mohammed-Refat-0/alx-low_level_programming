a34439eadd86689a0d01f086be55c68f53f95f0f
mohammedrefat <mohammedrefat53@outlook.com>
2023-09-05 23:22:17 +0300

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,63 +1,31 @@
 #include <stdlib.h>
-
+#include "main.h"
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ * Return: pointer to an array of strings
+ */
 char **strtow(char *str)
 {
-    char **ptr;
-    int i, j, k;
-    int words = 0;
+	char **ptr;
+	int i;
+	int words = 0;
 
-    if (!str || *str == '\0')
-    {
-        return NULL;
-    }
-    
-    for (i = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] == ' ')
-        {
-            words++;
-        }
-    }
-    
-    if (str[i - 1] != ' ')
-    {
-        words++;
-    }
-    
-    ptr = (char **) malloc((words + 1) * sizeof(char *));
-    if (!ptr)
-    {
-        return NULL;
-    }
-    
-    for (i = 0, j = 0, k = 0; str[i] != '\0'; i++)
-    {
-        if (str[i] == ' ')
-        {
-            ptr[j] = (char *) malloc((k + 1) * sizeof(char));
-            if (!ptr[j])
-            {
-                return NULL;
-            }
-            ptr[j][k] = '\0';
-            j++;
-            k = 0;
-        }
-        else
-        {
-            ptr[j][k] = str[i];
-            k++;
-        }
-    }
-    
-    ptr[j] = (char *) malloc((k + 1) * sizeof(char));
-    if (!ptr[j])
-    {
-        return NULL;
-    }
-    ptr[j][k] = '\0';
-    
-    ptr[words] = NULL;
-    
-    return ptr;
+	if (!str || *str == ' '  || *str == '\0')
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' && str[i - 1] != ' ')
+		{
+			words++;
+		}
+	}
+	ptr = (char **) malloc((words + 1) * sizeof(char *));
+	if (!ptr)
+	{
+		return (NULL);
+	}
+	return (ptr);
 }
