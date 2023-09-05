76aa23682a41e037f477d1b5add720bc63f43d00
mohammedrefat <mohammedrefat53@outlook.com>
2023-09-05 21:44:23 +0300

    task 101

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -7,9 +7,51 @@
  */
 char **strtow(char *str)
 {
+	char **ptr;
+	int i, j;
+	int words = 0;
+	int c = 0;
+
 	if (!str)
 	{
 		return (NULL);
 	}
 	return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == 32)
+		{
+			words++;
+		}
+	}
+	words++;
+	ptr = (char **) malloc((words + 1) * sizeof(char *));
+	if (!ptr)
+	{
+		return (NULL);
+	}
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == 32)
+		{
+			ptr[i] = (char *) malloc((c + 1) * sizeof(char));
+			if (!ptr[i])
+			{
+				return (NULL);
+			}
+			c = 0;
+		}
+		c++;
+	}
+	ptr[words] = NULL;
+
+	for (i = 0; ptr[i] != NULL; i++)
+	{
+		for (j = 0; str[j] != 32; j++)
+		{
+			ptr[i][j] = str[j];
+		}
+		ptr[i][j] = '\0';
+	}
+	return (ptr);
 }
