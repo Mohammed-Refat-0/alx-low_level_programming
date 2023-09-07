a835b795377146f5a7987965a45a4821417b5938
mohammedrefat <mohammedrefat53@outlook.com>
2023-09-07 21:25:30 +0300

    task 100

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,31 +5,32 @@
 /**
  * main - multiplies two numbers
  * @argc: number of arguments
- * @argv: argument strings
- * Return: int
+ * @argv: arguments strings
+ * Return: integer
  */
 int main(int argc, char *argv[])
 {
-	long mul;
 	int i, j;
-	
+	unsigned long muln;
+
 	if (argc != 3)
 	{
 		printf("Error\n");
+		exit(98);
 	}
-	exit(98); }
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  
+			{
 				printf("Error\n");
+				exit(98);
 			}
-			exit(98); }
 		}
+
 	}
-	mul = atol(argv[1]) *  atol(argv[2]);
-	printf("%l\n", mul);
+	muln = atol(argv[1]) *  atol(argv[2]);
+	printf("%lu\n", muln);
 	return (0);
 }
