3c6d8a339e60b2481cd968e89c63800d8fef822e
mohammedrefat <mohammedrefat53@outlook.com>
2023-09-07 17:49:17 +0300

    task 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,16 +15,26 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *ptr;
 
 	if (s1 == NULL)
+	{
 		s1 = "";
+	}
 	if (s2 == NULL)
+	{
 		s2 = "";
+	}
 	while (s1[length1] != '\0')
-	{length1++; }
+	{
+		length1++;
+	}
 	while (s2[length2] != '\0')
-	{length2++; }
+	{
+		length2++;
+	}
 	if (n >= length2)
-	{n = length2; }
-	ptr = malloc(sizeof(*ptr) * (length1 + n + 1));
+	{
+		n = length2;
+	}
+	ptr = malloc(sizeof(char) * (length1 + n + 1));
 	if (!ptr)
 	{
 		return (NULL);
