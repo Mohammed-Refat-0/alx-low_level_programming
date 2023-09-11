55588a85ea7da31d9e2ad7c2de8dae9015415196
mohammedrefat <mohammedrefat53@outlook.com>
2023-09-12 00:40:58 +0300

    task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,14 +9,37 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *dog = malloc(sizeof(dog_t));
+	int length1 = 0;
+	int length2 = 0;
+	dog_t *dog;
 
-	if (!dog || !name || !owner)
+	if (!name || !owner)
+	{
+		return (NULL);
+	}
+	while (name[length1] != '\0')
+	{
+		length1++;
+	}
+	while (owner[length2] != '\0')
+	{
+		length2++;
+	}
+	dog = malloc(sizeof(dog_t));
+	if (!dog)
+	{
+		return (NULL);
+	}
+	dog->name = malloc(sizeof(char) * (length1 + 1));
+	dog->owner = malloc(sizeof(char) * (length2 + 1));
+	if (!dog->owner || !dog->name)
 	{
 		return (NULL);
 	}
 	dog->name = name;
 	dog->age = age;
 	dog->owner = owner;
+	dog->name[length1 + 1] = '\0';
+	dog->owner[length2 + 1] = '\0';
 	return (dog);
 }
