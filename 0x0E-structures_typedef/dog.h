e4fb0c96a46d73a4096b170dd90e39448c458af7
mohammedrefat <mohammedrefat53@outlook.com>
2023-09-11 23:08:08 +0300

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef HEADER_FILE
+#define HEADER_FILE
+/**
+ * struct dog - a dog's data
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner's name
+ */
+struct dog
+{
+	 char *name;
+	 float age;
+	 char *owner;
+};
+
+#endif
