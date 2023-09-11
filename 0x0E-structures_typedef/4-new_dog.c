2afa7443ae1f7c81382bddfa30dabfe41f3a6e3c
mohammedrefat <mohammedrefat53@outlook.com>
2023-09-11 23:42:15 +0300

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,20 @@
+#include "dog.h"
+/**
+ * new_dog - create a new dog.
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: a pointer to struct dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog = malloc(sizeof(dog_t));
+	if (!dog || !name || !owner)
+	{
+		return (NULL);
+	}
+	dog->name = name;
+	dog->age = age;
+	dog->owner = owner;
+	return (dog);
+}
