ceff9f59eaab442d30c3c05c1e082cf2db34a52a
mohammedrefat <mohammedrefat53@outlook.com>
2023-09-13 19:45:00 +0300

    task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,6 +1,7 @@
 #include "function_pointers.h"
 #include <stdlib.h>
-/** print_name - a function that prints a name
+/**
+ * print_name - a function that prints a name
  * @name - pointer to the string to print
  * @(*f)(char *) - pointer to the function that prints thw string
  * to allow for different peint cases using differnt fn ptrs
