cdfce2e95bb5338847a15e0eec206e532626d2c4
mohammedrefat <mohammedrefat53@outlook.com>
2023-09-15 02:32:41 +0300

    task 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,11 +9,12 @@
 void print_all(const char * const format, ...)
 {
 	int i = 0;
-	char *s, *s1 = "";
+	char *str, *sep = "";
 
-	va_list ap;
+	va_list list;
 
-	va_start(ap, format);
+	va_start(list, format);
+
 	if (format)
 	{
 		while (format[i])
@@ -21,28 +22,29 @@ void print_all(const char * const format, ...)
 			switch (format[i])
 			{
 				case 'c':
-					printf("%s%c", s1, va_arg(ap, int));
+					printf("%s%c", sep, va_arg(list, int));
 					break;
 				case 'i':
-					printf("%s%d", s1, va_arg(ap, int));
+					printf("%s%d", sep, va_arg(list, int));
 					break;
 				case 'f':
-					printf("%s%f", s1, va_arg(ap, double));
+					printf("%s%f", sep, va_arg(list, double));
 					break;
 				case 's':
-					s = va_arg(ap, char*);
-					if (!s)
-						s = "(nil)";
-					printf("%s%s", s1, s);
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
 					break;
 				default:
 					i++;
 					continue;
 			}
-			s = ", ";
+			sep = ", ";
 			i++;
 		}
 	}
+
 	printf("\n");
-	va_end(ap);
+	va_end(list);
 }
