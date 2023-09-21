4ef4bcd2529de00d56fd3550d41bfe36e0fa4ccb
mohammedrefat <mohammedrefat53@outlook.com>
2023-09-21 10:07:03 +0300

    task 1

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * print_list - unction that prints all the elements of a list_t list.
+ * @h: head pointer of linked list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	int c;
+
+	if (!h)
+	{
+		return (1);
+	}
+	while (h->next != NULL)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("%s\n", h->str);
+		}
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
