e0cb484b032977db554d60d256ea6fba01849af2
mohammedrefat <mohammedrefat53@outlook.com>
2023-09-26 12:47:46 +0300

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -37,7 +37,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		if (i == idx)
 		{
 			newnode->next = temp->next;
-			newnode = temp;
+			temp->next = newnode;
 			return (newnode);
 		}
 		i++;
