c7bbf45a5041a2eab1c7d571e03dd499bbf89a0a
mohammedrefat <mohammedrefat53@outlook.com>
2023-09-28 17:45:58 +0300

    task 100

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * get_endianness - checks whether a cpu arch. is big or little endian
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+
+	char *e = (char *)&(i);
+	if (*e == 1)
+	{
+		return (1);
+	}
+	return (0);
+}
