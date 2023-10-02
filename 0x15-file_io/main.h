c7f862bfae9b9b9b3aca4ddcb47a63ae1197c93f
mohammedrefat <mohammedrefat53@outlook.com>
2023-10-02 15:44:30 +0300

    task 2

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,12 +1,10 @@
 #ifndef MYHEADERS_H
 #define MYHEADERS_H
-#include <elf.h>
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <fcntl.h>
-#include <sys/types.h>
-#include <sys/stat.h>
 
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
