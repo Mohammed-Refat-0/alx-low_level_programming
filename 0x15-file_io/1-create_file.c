24bdf563bc2b4b5bdcb975ad6d64c5c99fbec210
mohammedrefat <mohammedrefat53@outlook.com>
2023-10-02 15:22:21 +0300

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	}
 	if (text_content)
 	{
-		while (text_content)
+		while (*text_content)
 		{
 			checker = write(file, text_content, 1);
 			if (checker != 1)
