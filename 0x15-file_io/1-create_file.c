f29011715ca53ac1c4903c9bdd8f10430b3090c7
mohammedrefat <mohammedrefat53@outlook.com>
2023-10-02 15:02:31 +0300

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,7 +15,7 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 
-	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 600);
+	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	if (file < 0)
 	{
 		return (-1);
