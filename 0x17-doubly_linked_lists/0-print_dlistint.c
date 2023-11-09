832f2e77ad46994328fee3b598c5c3bcc50c859b
Mohammed Refat <mohammedrefat53@outlook.com>
2023-11-09 17:49:18 +0200

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * print_dlistint - function that prints all the elements in a doubly list.
+ * @h: head pointer of linked list
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h);
+{
+	int c = 0;
+
+	while (h->next != NULL)
+	{
+		printf("%i\n", h->n);
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
