c15e0fb4395cc0f4cc8ff401943eb7dc6c03ddd3
Mohammed Refat <mohammedrefat53@outlook.com>
2023-11-09 23:41:43 +0200

    task 4

--- 0x17-doubly_linked_lists/4-free_dlistint.c
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -7,16 +7,12 @@
 void free_dlistint(dlistint_t *head)
 {
 	dlistint_t *temp;
-
-	if (!(head))
-	{
-		return;
-	}
+    
 	temp = (head);
 	while ((head)->next != NULL)
 	{
-		temp = (head);
-		head = (head)->next;
+		temp = head;
+		head = head->next;
 		free(temp);
 	}
 }
