8a4a84adfe97aa4bbc64844ec3168f4647686c1f
Mohammed Refat <mohammedrefat53@outlook.com>
2023-11-09 23:56:53 +0200

    task 4

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"i
+/**
+ * *get_dnodeint_at_index - function that returns nth node
+ * @head: pointer to head pointer of linked list
+ * @index: index of node to return
+ * Return: node pointer
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	dlistint_t *temp;
+	unsigned int i = 0;
+
+	if (!head)
+	{
+		return (NULL);
+	}
+	temp = head;
+	while (temp)
+	{
+		if (i == index)
+		{
+			return (temp);
+		}
+		temp = temp->next;
+	}
+	return (NULL);
+}
