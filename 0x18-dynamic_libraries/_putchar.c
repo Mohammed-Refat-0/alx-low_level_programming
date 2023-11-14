243a5f1a2469ff17fbede1c4f948b7652574f246
Mohammed Refat <mohammedrefat53@outlook.com>
2023-11-14 03:57:23 +0200

    task 0

--- 0x18-dynamic_libraries/_putchar.c
+++ 0x18-dynamic_libraries/_putchar.c
@@ -1,4 +1,6 @@
+#include <unistd.h>
 #include "main.h"
+
 /**
  * _putchar - writes the character c to stdout
  * @c: The character to print
@@ -8,4 +10,5 @@
  */
 int _putchar(char c)
 {
+return (write(1, &c, 1));
 }
