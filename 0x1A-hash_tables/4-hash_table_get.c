dbd2624c4bc008fc1ada4f2e2e7096dca92468e1
Mohammed Refat <mohammedrefat53@outlook.com>
2023-12-01 16:35:18 +0200

    task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -3,14 +3,18 @@
  * hash_table_get -  retrieves a value associated with a key.
  * @ht: pointer to hash table
  * @key: key to get its asossiated value
+ * Return: value if found, null otherwise
  */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned int index;
 	hash_node_t *node;
 
+	if (ht == NULL)
+	{
+		return (NULL);
+	}
 	index = key_index((const unsigned char *)key, ht->size);
-
 	node = ht->array[index];
 
 	while (node != NULL)
