456c47274ec2ba29a95adcc499ff9ab1b9224206
Mohammed Refat <mohammedrefat53@outlook.com>
2023-12-01 16:32:41 +0200

    task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+ * hash_table_get -  retrieves a value associated with a key.
+ * @ht: pointer to hash table
+ * @key: key to get its asossiated value
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned int index;
+	hash_node_t *node;
+
+	index = key_index(key, ht->size);
+
+	node = ht->array[index];
+
+	while (node != NULL)
+	{
+		if (strcmp(node->key, key) == 0)
+		{
+			return (node->value);
+		}
+		node = node->next;
+	}
+	return (NULL);
+}
