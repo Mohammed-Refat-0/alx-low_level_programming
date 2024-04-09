116f97825d113ee56dce7868b2e2e50498cdb3d4
Mohammed Refat <mohammedrefat53@outlook.com>
2024-04-09 16:50:16 +0200

    tasks

---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,32 @@
+#include "search_algos.h"
+
+/**
+ * binary_search - searches for a value in an array of integers
+ *  using Linear search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: size of the array
+ * @value: the value to search for
+ * Return: the index of the value in the array, -1 otherwise
+ **/
+int binary_search(int *array, size_t size, int value)
+{
+
+	int index;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+
+	for (index = 0; index < (int)size; index++)
+	{
+		printf("Value checked array[%d] = [%d]\n", index, array[index]);
+
+		if (array[index] == value)
+		{
+			return (index);
+		}
+	}
+
+	return (-1);
+}
