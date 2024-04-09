829006cbe759b488b79609ab2277763ae5896fca
Mohammed Refat <mohammedrefat53@outlook.com>
2024-04-09 14:38:20 +0200

    task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,27 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of integers
+ *  using Linear search algorithm
+ * @array: pointer to the first element of the array to search in
+ * @size: size of the array
+ * @value: the value to search for
+ * Return: the index of the value in the array, -1 otherwise
+ **/
+int linear_search(int *array, size_t size, int value)
+{
+
+	int index;
+
+	for (index = 0; index < size; index++)
+	{
+		if (array[index] == value)
+		{
+			printf("%d/n", array[index]);
+			return (index);
+		}
+		printf("%d/n", array[index]);
+	}
+
+	return (-1);
+}
---
+++ 0x1E-search_algorithms/README.md
@@ -0,0 +1,1 @@
+This repo contains implementations of various search algorithms, such as linear search and binary search
